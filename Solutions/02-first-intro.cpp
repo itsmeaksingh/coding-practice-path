dcfc34644fe35a19a013f130848252f3db24e286
Amit_Kumar <52384184+Darktechie-programmer@users.noreply.github.com>
2022-06-26 21:44:41 +0530

    Add files via upload

--- Solutions/02-first-intro.cpp
+++ Solutions/02-first-intro.cpp
@@ -8,7 +8,7 @@ find Simple Interest.  --> easy p*t*r/100
 (Use if conditions)
 5. Take 2 numbers as input and print the largest number.  --> easy
 6. Input currency in rupees and output in USD.  --> easy
-7. To calculate Fibonacci Series up to n numbers. 
+7. To calculate Fibonacci Series up to n numbers.
 8. To find out whether the given String is Palindrome or not.
 9. To find Armstrong Number between two given number.
 */
@@ -16,10 +16,48 @@ find Simple Interest.  --> easy p*t*r/100
 #include <bits/stdc++.h>
 using namespace std;
 
+void fibonacciNumber(int n)
+{
+    int a = 0;
+    int b = 1;
+    int c = 0;
+    for (int i = a; i < n; i++)
+    {
+        cout << a << " ";
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
 
+bool checkPalindromeNumber(int n)
+{
+    string str = to_string(n);
+    reverse(str.begin(), str.end());
+    cout << str << endl;
+    return str == to_string(n);
+}
+
+bool getArmstrongNumber(int n)
+{   
+    int len = to_string(n).length();
+    int res;
+    long long int sum = 0;
+    int temp = n;
+    while (n > 0)
+    {
+        res = n % 10;
+        sum += pow(res, len);
+        n /= 10;
+    }
+    cout << sum << endl;
+    return sum == temp;
+}
 
 int main()
 {
-
+    fibonacciNumber(5);
+    cout << checkPalindromeNumber(1245421) << endl;
+    cout << getArmstrongNumber(370) << endl;
     return 0;
 }
